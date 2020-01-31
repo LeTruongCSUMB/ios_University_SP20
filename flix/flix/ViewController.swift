09ce73ec6a4f7834c2226f6f727b2b012729a6b8
Lewis Truong <letruong@csumb.edu>
2020-01-30 17:46:13 -0800

    Flixer
    Assignment 1 of iOS university, without the alamofireimage due to a lack of administrative access.

---
+++ flix/flix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Lewis Truong on 1/30/20.
+//  Copyright © 2020 Lewis Truong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
