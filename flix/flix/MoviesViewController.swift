72fbea36058b6b5e9be4b3e920818ce124d49d34
Lewis Truong <letruong@csumb.edu>
2020-02-04 18:25:11 -0800

    Finished Flix Part 1

--- flix/flix/MoviesViewController.swift
+++ flix/flix/MoviesViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-//import AlamofireImage
+import AlamofireImage
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -65,7 +65,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let posterPath = movie["poster_path"] as! String
         let posterUrl = URL(string: baseURL + posterPath)
         
-        //cell.posterView.af_setImage(withURL: posterUrl!)
+        cell.posterView.af_setImage(withURL: posterUrl!)
         
         return cell
     }
