fc45e208be92ce2a6c634902f5956b5fc0f330f5
Lewis Truong <letruong@csumb.edu>
2020-02-06 19:43:08 -0800

    Completed Assignment 2 of Flix

---
+++ flix/flix/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Jerry Kankelborg on 2/6/20.
+//  Copyright © 2020 Lewis Truong. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
